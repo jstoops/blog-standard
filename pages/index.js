3af9018d476f3d24022d89c411f5417e6500235c
John Stoops <john.stoops@gmail.com>
2025-03-05 16:22:55 -1000

    Set auth state & user info if logged in

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
 
 export default function Home() {
+  const { user } = useUser();
+
+  console.log('USER', user);
+
   return (
     <div>
       <h1>Homepage</h1>
       <div>
-        <Link href='/api/auth/login'>Login</Link>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                width={50}
+                height={50}
+              />
+              <div>{user.email}</div>
+            </div>
+            <Link href='/api/auth/logout'>Logout</Link>
+          </>
+        ) : (
+          <Link href='/api/auth/login'>Login</Link>
+        )}
       </div>
     </div>
   );
