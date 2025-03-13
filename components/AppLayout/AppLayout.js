2e5b765f81e5130245036ae22bdcbd7f0b97757f
John Stoops <john.stoops@gmail.com>
2025-03-12 15:33:40 -1000

    Create sidebar & app logo

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,57 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Logo } from '../Logo/Logo';
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser();
+
   return (
-    <div>
-      this is the app layout<div>{children}</div>
+    <div className='grid grid-cols-[300px_1fr] h-screen max-h-screen'>
+      <div className='flex flex-col text-white overflow-hidden'>
+        <div className='bg-slate-800 px-2'>
+          <Logo />
+          <Link
+            href='/post/new'
+            className='bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block'
+          >
+            New post
+          </Link>
+          <Link href='/token-topup' className='block mt-2 text-center'>
+            <FontAwesomeIcon icon={faCoins} className='text-yellow-500' />
+            <span className='pl-1'>0 tokens available</span>
+          </Link>
+        </div>
+        <div className='flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800'>
+          list of posts
+        </div>
+        <div className='bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2'>
+          {!!user ? (
+            <>
+              <div className='min-w-[50px]'>
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  width={50}
+                  height={50}
+                  className='rounded-full'
+                />
+              </div>
+              <div className='flex-1'>
+                <div className='font-bold'>{user.email}</div>
+                <Link className='text-sm' href='/api/auth/logout'>
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href='/api/auth/login'>Login</Link>
+          )}
+        </div>
+      </div>
+      <div className=''>{children}</div>
     </div>
   );
 };
