9aae56a11d7b1467192588e78df04e6b7bb66802
John Stoops <john.stoops@gmail.com>
2025-03-15 11:06:30 -1000

    Setup MongoDB, save available tokens for a user & save generated blog posts

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,18 +1,34 @@
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import { OpenAIApi, Configuration } from 'openai';
+import clientPromise from '../../lib/mongodb';
 
-export default async function handler(req, res) {
+export default withApiAuthRequired(async function handler(req, res) {
+  const aiModel = 'gpt-4o-mini';
+
   try {
+    const { user } = await getSession(req, res);
+    const client = await clientPromise;
+    const db = client.db('blogstandard');
+
+    const userProfile = await db.collection('users').findOne({
+      auth0Id: user.sub,
+    });
+
+    if (!userProfile?.availableTokens) {
+      res.status(403);
+      return;
+    }
+
     const config = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
 
     const openai = new OpenAIApi(config);
 
-    const topic = 'cat ownership';
-    const keywords = 'first time cat owner, kitten diet';
+    const { topic, keywords } = req.body;
 
     const response = await openai.createChatCompletion({
-      model: 'gpt-4o-mini-2024-07-18',
+      model: aiModel,
       messages: [
         {
           role: 'system',
@@ -35,11 +51,60 @@ export default async function handler(req, res) {
       ],
     });
 
-    res
-      .status(200)
-      .json({ postContent: response.data.choices[0]?.message?.content });
+    const postContent = response.data.choices[0]?.message?.content;
+
+    const seoResponse = await openai.createChatCompletion({
+      model: aiModel,
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are an SEO friendly blog post generator called BlogStandard. You are designed to output JSON. Do not include HTML tags in your output',
+        },
+        {
+          role: 'user',
+          content: `
+            Generate an SEO friendly title and SEO friendly meta description for the following blog post.
+            ${postContent}
+            ---
+            The output JSON must be in the following format:
+            {
+              "title": "example title",
+              "metaDescription": "example meta description"
+            }
+          `,
+        },
+      ],
+      response_format: { type: 'json_object' },
+    });
+
+    const { title, metaDescription } =
+      JSON.parse(seoResponse.data.choices[0]?.message?.content) || {};
+
+    await db.collection('users').updateOne(
+      {
+        auth0Id: user.sub,
+      },
+      {
+        $inc: {
+          availableTokens: -1,
+        },
+      }
+    );
+
+    const post = await db.collection('posts').insertOne({
+      postContent,
+      title,
+      metaDescription,
+      topic,
+      keywords,
+      userId: userProfile._id,
+      created: new Date(),
+    });
+
+    res.status(200).json({ post: { postContent, title, metaDescription } });
   } catch (error) {
-    console.error(error.response.data ?? error.message);
-    res.status(500).json({ error: error.response.data ?? error.message });
+    console.error(error.response?.data ?? error.message);
+    res.status(500).json({ error: error.response?.data ?? error.message });
   }
-}
+});
--- pages/post/new.js
+++ pages/post/new.js
@@ -4,21 +4,51 @@ import { useState } from 'react';
 import Markdown from 'react-markdown';
 
 export default function NewPost(props) {
+  const [topic, setTopic] = useState('');
+  const [keywords, setKeywords] = useState('');
   const [postContent, setPostContent] = useState('');
 
-  const handleClick = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const response = await fetch(`/api/generatePost`, {
       method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ topic, keywords }),
     });
     const json = await response.json();
-    setPostContent(json.postContent);
+    setPostContent(json.post.postContent);
+    console.log(json.post.title);
+    console.log(json.post.metaDescription);
   };
   return (
     <div>
-      <h1>New Post Page</h1>
-      <button className='btn' onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className='resize=none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className='resize=none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          />
+        </div>
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
       <Markdown>{postContent}</Markdown>
     </div>
   );
